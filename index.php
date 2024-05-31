2c635d9ebe02cef5f1f210730d9fa3ff1bc2f507
hrazdiraa <144205425+hrazdiraa@users.noreply.github.com>
2024-05-31 23:04:19 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php include 'header.php'; ?>
+<div class="main">
+  <div class="centerContent">
+    <div class="content">
+      <h1>
+        3D Tiskárna<br /><span>Original Prusa i3 MK3S+</span> <br /><br />
+      </h1>
+      <p class="par">
+        Představujeme Vám webovou stránku pro 3D tisk na tiskárnu Original
+        Prusa i3 MK3S+. <br />
+        Zde Vám umožníme vytisknout si své modely a poradíme s jejich
+        realizací.
+        <br />
+        Zajímá Vás více informací? Projděte si naše webové stránky
+      </p>
+
+      <button class="cn"><a href="aboutPrinter.php">MORE</a></button>
+    </div>
+  </div>
+</div>
+<?php include 'footer.php'; ?>
