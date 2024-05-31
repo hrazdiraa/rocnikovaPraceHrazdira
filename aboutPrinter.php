e8945c7d285ee3fdcf1c7a78f8bb2c02da5c288a
hrazdiraa <144205425+hrazdiraa@users.noreply.github.com>
2024-05-31 23:04:49 +0200

    Add files via upload

---
+++ aboutPrinter.php
@@ -0,0 +1,26 @@
+<?php include 'header.php'; ?>
+<div class="main">
+  <div class="centerContent">
+    <div class="content2">
+      <h1>
+        3D Tiskárna<br /><span>Original Prusa i3 MK3S+</span> <br /><br />
+      </h1>
+      <p class="par">
+        Original Prusa i3 MK3S+ je nejnovější verzí naší oceňované 3D
+        tiskárny. Verzi MK3S jsme obohatili o novou sondu SuperPINDA, pomocí
+        které je možné dosáhnout lepší kalibrace první vrstvy. Dále jsme
+        přidali kvalitní ložiska a tiskárnu jsme na různých místech lehce
+        vylepšili, aby se lépe skládala a servisovala. MK3S+ obsahuje
+        všechny kumulativní vylepšení z celého životního cyklu MK3.<br /><br />
+        Najdete tu proto vyhřívanou podložku s odnímatelnými pružnými
+        tiskovými pláty s PEI povrchem, dále je tu automatický Mesh Bed
+        Leveling pro vyrovnání nerovostí podložky, senzor filamentu,
+        zotavení ze ztráty napájení (tzv. Power Panic) a řada důležitých
+        bezpečnostních prvků. A tiskárna je stále nádherně tichá! Funkce
+        naší MK3S+ lze dále rozšířit pomocí unikátního řešení Multi-Material
+        Upgrade 2S, s pomocí kterého lze tisknout až pěti filamenty zároveň.
+      </p>
+    </div>
+  </div>
+</div>
+<?php include 'footer.php'; ?>
---
+++ contact.php
@@ -0,0 +1,19 @@
+<?php include 'header.php'; ?>
+<div class="main">
+  <div class="centerContent">
+    <div class="content5">
+      <h1>Contact</h1>
+      <hr />
+      <div class="imagesC">
+        <div class="card">
+          <img class="contactImg" src="public/img/hrazdira.JPG" alt="karta1" />
+          <div class="container">
+            <h4><b>Oliver Hrazdíra</b></h4>
+            <p>Architect & Engineer<br />email: hrazdira@spsejecna.cz</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<?php include 'footer.php'; ?>
